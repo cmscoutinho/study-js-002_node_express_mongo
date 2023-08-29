214ce68ce6b2853cff07ecf8a6b3df62110a9c01
Claudio Coutinho <ccoutinho.ti@gmail.com>
2023-08-29 15:23:28 -0300

    Created app.js and configured it to return a JSON with books (livros).

--- alura-node/server.js
+++ alura-node/server.js
@@ -1,19 +1,7 @@
-const http = require("http");
-const port = 3000;
+import app from "./src/app.js";
 
-const rotas = {
-  "/": "Curso de Node.",
-  "/livros": "Acessando pagina de livros.",
-  "/autores": "Listagem de autores.",
-  "/editora": "Página da editora.",
-  "/sobre": "Informacoes sobre a pagina.",
-};
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-type": "text/plain" });
-  res.end(rotas[req.url]);
-});
-
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Escutando em http://localhost:${port}`);
 });
---
+++ alura-node/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+const livros = [
+  { id: 1, titulo: "O Senhor dos Aneis" },
+  { id: 2, titulo: "O Hobbit" },
+];
+
+app.get("/", (req, res) => {
+  res.status(200).send("Curso de Node.js");
+});
+
+app.get("/livros", (req, res) => {
+  res.status(200).json(livros);
+});
+
+export default app;
