c7e5a0ab450570c71a6620deaec201529dc07b85
Claudio Coutinho <ccoutinho.ti@gmail.com>
2023-09-02 14:47:09 -0300

    Imported mongoose connection to 'app.js' and tested it successfully.

--- alura-node/config/dbConnect.js
+++ alura-node/config/dbConnect.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 let pass = 123;
 
 mongoose.connect(`mongodb+srv://claudio:${pass}@alura.bn0fdjr.mongodb.net/`);
-let db = mongoose.connection();
+
+let db = mongoose.connection;
 
 export default db;
